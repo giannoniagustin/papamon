41052f59e0f9b93e0871e7a11968c544b75d47b1
Agustin Giannoni <agustin.giannoni@finconsgroup.com>
2023-08-20 09:45:33 +0200

    Add catch exception request

--- controllers/MasterController.py
+++ controllers/MasterController.py
@@ -19,10 +19,15 @@ class MasterController:
             url= EndPoint.url_template.format(rB.ip,rB.port,EndPoint.IMAGE) #EndPoint.buildUrl(rB.ip,EndPoint.IMAGE,rB.port)
             print("Url RB ",url)
             params = {f"data": {request_id}}
-            response = requests.get(url,params=params)
-            if response.status_code == 200:
-                imageFile=Paths.BUILD_IMAGE_FILE.format(request_id,request_id,rB.id,".jpg")
-                File.FileUtil.writeImage(imageFile,response.content)
-                print(f"Imagen descargada y almacenada con éxito de RB {rB.name} ")
-            else:
-                print(f"Error al descargar la imagen de RB {rB.name}")
+            try:
+                response = requests.get(url,params=params)
+                if response.status_code == 200:
+                    imageFile=Paths.BUILD_IMAGE_FILE.format(request_id,request_id,rB.id,".jpg")
+                    File.FileUtil.writeImage(imageFile,response.content)
+                    print(f"Imagen descargada y almacenada con éxito de RB {rB.name} ")
+                else:
+                    print(f"Error al descargar la imagen de RB {rB.name}")
+            except requests.exceptions.ConnectionError as e:
+                    print("Error de conexión:", e)
+            except requests.exceptions.RequestException as e:
+                    print("Error en la solicitud:", e)
