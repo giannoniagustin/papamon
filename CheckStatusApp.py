3496f2f7cefb16b199aa62b7849908f395e9e386
agustingiannoni <agustin.giannoni@finconsgroup.com>
2023-10-24 14:08:06 +0200

    scheduler images

--- CheckStatusApp.py
+++ CheckStatusApp.py
@@ -1,25 +1,25 @@
 #!/usr/bin/env python3
 '''Agregar al servicio para que se ejcute cada vez que se inicia el servidor'''
 
-import Api
-
 from util.Sentry import Sentry
 from controllers.MasterController import MasterController
 from controllers.scheduler.SchedulerController import SchedulerController
 import time as time
 import schedule
+import constants.Paths as Paths
+
 
 def checkStatus(): 
       MasterController.getStatus()
 def processCheckStatus():
      Sentry.init()
      Sentry.customMessage(filename=None,path=None,eventName="Inicio de Sentry ProcessCheckStatus ")  
-     SchedulerController.build(job=checkStatus)
+     SchedulerController.build(job=checkStatus,pathScheduler=Paths.SCHEDULER_STATUS)
      while True:
       schedule.run_pending()
       time.sleep(1)
 
 if __name__ == '__main__':
     Sentry.init()
-    MasterController.getStatus()
+    #MasterController.getStatus()
     processCheckStatus()
--- MasterApp.py
+++ MasterApp.py
@@ -24,47 +24,26 @@ def initApp():
    Sentry.init()
    Sentry.customMessage(Paths.ME_FILE,Paths.ME,f"Inicio de App Master {datetime.datetime.now()}")   
 
-def checkStatus(): 
-      MasterController.getStatus()
-      
-
     
 def main():
    initApp()
-   checkStatus()
    sentry_sdk.capture_message(f"Fin de App Master {datetime.datetime.now()}") 
-def scheduler():
-        # Crear objetos time
-    hora1 = Time(10, 30, 00)
-    hora2 = Time(15, 45, 00)
-    hora3 = Time(23, 15, 00)
-    # Crear una lista de objetos time
-    lista_horas = [hora1, hora2, hora3]
-    scheduler = Scheduler(monday=lista_horas,tuesday=lista_horas,wednesday=lista_horas,thursday=lista_horas,friday=lista_horas,saturday=lista_horas,sunday=lista_horas)
-    SchedulerMapper().toJson(scheduler)
-    schedulerController = SchedulerController()
-    schedulerController.update(scheduler)
-    
-    #print(schedulerController.get())
-def job():
-    print(f"I'm working... {datetime.datetime.now()}")
-
-      
-def processCheckStatus():
-     Sentry.init()
-     Sentry.customMessage(filename=None,path=None,eventName="Inicio de Sentry en proceso ")  
-     SchedulerController.build(job=checkStatus)
-     while True:
-      schedule.run_pending()
-      time1.sleep(1)
-def checkCallTakeImage():
-    ApiController.callTakeImage("")
+   
 def callReconstruct():
     if (MasterController.getImages()):
         print(f"Reconstruccion exitosa: {datetime.datetime.now()}")
         
     else:
-        print(f"Reconstruccion fallida,puede haber generado alguna imagen: {datetime.datetime.now()}")    
+        print(f"Reconstruccion fallida,puede haber generado alguna imagen: {datetime.datetime.now()}")   
+def getImages(): 
+      MasterController.getImages()
+def processGetImages():
+     Sentry.init()
+     Sentry.customMessage(filename=None,path=None,eventName="Inicio de Sentry ProcessGetImages ")  
+     SchedulerController.build(job=getImages,pathScheduler=Paths.SCHEDULER_GET_IMAGES)
+     while True:
+      schedule.run_pending()
+      time1.sleep(1) 
           
 if __name__ == "__main__":
     
@@ -74,7 +53,9 @@ if __name__ == "__main__":
     print("#################################################################")
     
     initApp()
-    callReconstruct()
+    processGetImages()
+    #callReconstruct()
+    
     #Api.app.run(host='0.0.0.0', port=meRaspb.port)
     
     
