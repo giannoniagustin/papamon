265ba4fe36426abe9384592b0171072e94da4eef
agustingiannoni <agustin.giannoni@finconsgroup.com>
2023-11-06 10:04:50 +0100

    config script

--- config/service/config.sh
+++ config/service/config.sh
@@ -4,10 +4,14 @@ if [ "$1" == "slave" ]; then
     echo "Configurando el servicio PapamonApp Slave"
     sudo systemctl enable papamonApp.service
     sudo systemctl disable papamonMasterApp.service
+    sudo systemctl daemon-reload
+    sudo systemctl start papamonApp.service
 elif [ "$1" == "master" ]; then
     echo "Configurando el servicio PapamonMasterApp Master.."
     sudo systemctl enable papamonMasterApp.service
     sudo systemctl disable papamonApp.service
+    sudo systemctl daemon-reload
+    sudo systemctl start papamonMasterApp.service
 else
     echo "Uso: $0 [papamon|master]"
 fi
