9e8480c38895b347ed3da417cb1af8b97495fe81
agustingiannoni <agustin.giannoni@finconsgroup.com>
2023-11-30 12:29:49 +0100

    update script

--- config/service/configServiceSlave.sh
+++ config/service/configServiceSlave.sh
@@ -4,6 +4,19 @@
 
 directorio="/home/papamon/Documents/papamon"
 nombre_servicio="papamonApp.service"  # Nombre del archivo de servicio
+
+# Función para obtener el valor de -name si se proporciona
+get_name_param() {
+    while [[ "$#" -gt 0 ]]; do
+        if [ "$1" == "-name" ]; then
+            nombre_servicio="$2"
+            return 0
+        fi
+        shift
+    done
+    return 1
+}
+
 has_start_param() {
     for var in "$@"; do
         if [ "$var" == "start" ]; then
@@ -20,11 +33,11 @@ has_stop_param() {
     done
     return 1
 }
+get_name_param "$@";then
 echo "Configurando el servicio PapamonApp $nombre_servicio"
 
 if  has_start_param  "$@" ; then
     echo "Configurando Inicio de servicio PapamonApp $nombre_servicio"
-        
     sudo systemctl daemon-reload
     sudo systemctl stop $nombre_servicio
     sudo systemctl enable $nombre_servicio
@@ -35,5 +48,8 @@ elif has_stop_param  "$@"; then
     sudo systemctl stop $nombre_servicio
     sudo systemctl disable $nombre_servicio
     sudo systemctl status $nombre_servicio
+fi
+else
+    echo "No se ha proporcionado el parametro -name"
 fi
 
