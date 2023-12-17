ea9e60ae75297be638a08ffa6407aa889452c65d
gestionpapamon <gestionpapamon@gmail.com>
2023-12-17 09:18:20 +0100

    syncRClon

--- config/service/syncRClone.sh
+++ config/service/syncRClone.sh
@@ -2,17 +2,13 @@
 
 if ping -q -c 1 -W 1 8.8.8.8 >/dev/null; then
     # Ejecutar el comando si hay conexión a internet
-    echo "Hay conexión a internet."
-    if pidof -o %PPID -x syncRClone.sh”; then
-        echo "Ya se esta ejutando el script de una ejecucion anterior"
-        exit 1
-    else
-        sudo rclone sync /home/papamon/Documents/out_reconstruct drive:/out_reconstruct -P
-        file=/home/papamon/Documents/out_reconstruct/log/rclone.log -v 
-fi
+        echo "Hay conexión a internet."
+        mkdir -p /home/papamon/Documents/out_reconstruct/log/
+        rclone sync /home/papamon/Documents/out_reconstruct drive:/out_reconstruct -P -v --log-file /home/papamon/Documents/out_reconstruct/log/rclone.txt
+        exit
+
 else
     # Puedes poner aquí un mensaje o alguna acción alternativa
     echo "No hay conexión a internet."
     exit
-
 fi
